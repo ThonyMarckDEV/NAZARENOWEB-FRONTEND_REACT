5d44689735931c51ae902e1d438aa56660cadc0c
ThonyMarckDEV <thonymarckyt08@gmail.com>
2024-10-01 21:30:10 -0500

    FIRST COMMIT

---
+++ frontend/src/Interfaces/Login.jsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Link } from 'react-router-dom';  // Importa Link aquí
+//import '../styles/Login/LoginPC.css';
+//import '../styles/Login/LoginMobile.css';
+
+function Login() {
+  
+}
+
+export default Login;
