4b2a6d76b84b9d44519be42a116add8d32847b01
ThonyMarckDEV <thonymarckyt08@gmail.com>
2024-10-01 21:50:25 -0500

    COOMIR BBOOT

---
+++ backend/nazarenoweb/src/main/java/com/cebe/nazarenoweb/NazarenowebApplication.java
@@ -0,0 +1,13 @@
+package com.cebe.nazarenoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NazarenowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NazarenowebApplication.class, args);
+	}
+
+}
